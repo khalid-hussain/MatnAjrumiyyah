a40804d0b03afc8907676d239fc8dffacc8279f2
Khalid <mohammad.khalid.hussain@gmail.com>
2015-03-12 10:20:11 +0800

    Added basic functionality

---
+++ app/src/main/java/com/khalid/ajru/Model/Chapter.java
@@ -0,0 +1,38 @@
+package com.khalid.ajru.Model;
+
+/**
+ * Created by theB75 on 09-Mar-15.
+ */
+public class Chapter {
+    String chapterTitle;
+    String href;
+
+    public Chapter(){
+    }
+
+    public Chapter(String chapterTitle, String href) {
+        this.chapterTitle = chapterTitle;
+        this.href = href;
+    }
+
+    public String getChapterTitle() {
+        return chapterTitle;
+    }
+
+    public void setChapterTitle(String chapterTitle) {
+        this.chapterTitle = chapterTitle;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    @Override
+    public String toString() {
+        return chapterTitle;
+    }
+}
--- app/src/main/java/com/khalid/ajru/ReaderActivity.java
+++ app/src/main/java/com/khalid/ajru/ReaderActivity.java
@@ -10,27 +10,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import com.khalid.ajru.Model.Book;
+import com.khalid.ajru.Model.Chapter;
+
+import java.util.ArrayList;
+
+public class ReaderActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private ListView leftDrawerList;
-    private ArrayAdapter<String> navigationDrawerAdapter;
-    private String[] leftSliderData = {"باب الكلام", "Android", "Sitemap", "About", "Contact Me"};
+    private ArrayAdapter<Chapter> navigationDrawerAdapter;
+    private ArrayList<Chapter> leftSliderData;
+    private WebView webView;
+    private Book book;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(Build.VERSION.SDK_INT >= 17){
+        book = new Book();
+        book.setBook(this, "sample_book");
+        leftSliderData = book.getChapterList();
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/sample_book/foreword.html");
+
+        if (Build.VERSION.SDK_INT >= 17) {
             getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
         }
-        
+
         initView();
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -43,24 +61,29 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerLayout.setStatusBarBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        navigationDrawerAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, leftSliderData);
+        navigationDrawerAdapter = new ArrayAdapter<>(ReaderActivity.this, android.R.layout.simple_list_item_1, leftSliderData);
         leftDrawerList.setAdapter(navigationDrawerAdapter);
+        leftDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String href = book.getChapterList().get(position).getHref();
+                Toast.makeText(getApplicationContext(), "Clicked me " + href, Toast.LENGTH_SHORT).show();
+                webView.loadUrl("file:///android_asset/sample_book/" + href);
+            }
+        });
     }
 
     private void initDrawer() {
-
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-
             }
         };
         drawerLayout.setDrawerListener(drawerToggle);
